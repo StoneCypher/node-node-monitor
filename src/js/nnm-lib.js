6561178516e56fc6d975ce8bde6d02b2938e637e
John Haugeland <stonecypher@gmail.com>
2017-03-02 21:59:56 -0800

    promise driven metrics timer

--- src/js/nnm-lib.js
+++ src/js/nnm-lib.js
@@ -23,7 +23,19 @@ const promiseLater = async (time, index, behavior, context) => {
 
 
 
-const llog = (context, index, resolve, reject) => { console.log(index + ': ' + context); resolve(); }
+const cpu = () => new Promise( (resolve, reject) => resolve('cpu auto-pass') );
+const ram = () => new Promise( (resolve, reject) => resolve('ram auto-pass') );
+
+const promiseLookup = { cpu, ram };
+
+const getMetrics = (whichMetrics = ['cpu','ram']) => whichMetrics.map(key => promiseLookup[key]);
+const runMetrics = (whichMetrics = ['cpu','ram']) => whichMetrics.map(key => promiseLookup[key]());
+
+
+
+
+
+const llog = (context, index, resolve, reject) => { console.log(index + ': ' + context); resolve(1); }
 const flog = (context, index, resolve, reject) => { console.log(index + ': ' + context); reject(); }
 
 
@@ -45,7 +57,7 @@ const boundedAll = promiseArray => {
     return Promise.all(promiseArray).then( results => {
 
         const ended_at = (new Date()).getTime();
-        return { start: started_at, end: ended_at, results: results };
+        return { start: started_at, end: ended_at, time: ended_at - started_at, results: results };
 
     } );
 
@@ -57,8 +69,7 @@ const boundedAll = promiseArray => {
 
 const monitorTick = config =>
 
-    new Promise(
-        (resolve, reject) => {
+    boundedAll(config.promises).then(res => console.log('Result:\n------\n\n' + JSON.stringify(res)));
 
-        }
-    );
+const uTick = () => monitorTick({ promises: manyLaterPromises(25, 1500, 5000, llog, 'abc') });
+const vTick = () => monitorTick({ promises: runMetrics() });
